4c351b12cb150a60b68b7603521d242e055938ad
jxom <7336481+jxom@users.noreply.github.com>
2025-10-01 14:10:41 +1000

    chore: build

--- dist/viem/index.js
+++ dist/viem/index.js
@@ -1,7 +1,7 @@
 export * from "./abis.js";
-export { decorator as tempoActions } from "./decorator.js";
 export * from "./chain.js";
 export * from "./client.js";
+export { decorator as tempoActions } from "./decorator.js";
 export * from "./formatters.js";
 export * from "./serializers.js";
 //# sourceMappingURL=index.js.map
--- src/viem/index.ts
+++ src/viem/index.ts
@@ -1,6 +1,6 @@
 export * from './abis.js'
-export { decorator as tempoActions } from './decorator.js'
 export * from './chain.js'
 export * from './client.js'
+export { decorator as tempoActions } from './decorator.js'
 export * from './formatters.js'
 export * from './serializers.js'
