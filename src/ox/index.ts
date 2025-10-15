a3f647de1acab970bb4c261e5537ce751a4f8606
jxom <7336481+jxom@users.noreply.github.com>
2025-10-15 12:14:52 +1100

    wip: u

--- src/ox/index.ts
+++ src/ox/index.ts
@@ -1,3 +1,4 @@
+export * as SignatureEnvelope from './SignatureEnvelope.js'
 export * as TokenId from './TokenId.js'
 export * as TokenRole from './TokenRole.js'
 export * as Transaction from './Transaction.js'
