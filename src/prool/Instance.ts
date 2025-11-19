27241b0c3d7ae37d77e7fe7ea4b4380a638a8c39
Daniel Gorrie <dgorrie@tempo.xyz>
2025-11-19 12:01:35 -0500

    breaking: rename prool Instance `facuet.address` -> `faucet.addresses` and update type to array

--- src/prool/Instance.ts
+++ src/prool/Instance.ts
@@ -33,7 +33,7 @@ export const tempo = defineInstance((parameters: tempo.Parameters = {}) => {
   const { deadline = 3, gaslimit = 3000000000, maxTasks = 8 } = builder ?? {}
   const { blockTime = '1sec' } = dev ?? {}
   const {
-    address = '0x20c0000000000000000000000000000000000001',
+    addresses = ['0x20c0000000000000000000000000000000000001'],
     amount = '1000000000000',
     privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
   } = faucet ?? {}
@@ -103,7 +103,7 @@ export const tempo = defineInstance((parameters: tempo.Parameters = {}) => {
                   blockTime,
                 },
                 faucet: {
-                  address,
+                  address: addresses,
                   amount,
                   privateKey,
                 },
@@ -201,9 +201,9 @@ export declare namespace tempo {
     faucet?:
       | {
           /**
-           * Target token address for the faucet to be funding with
+           * Target token addresses for the faucet to be funding with
            */
-          address?: string | undefined
+          addresses?: string[] | undefined
           /**
            * Amount for each faucet funding transaction
            */
