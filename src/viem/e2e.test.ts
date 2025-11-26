0428140fe521193a8a5b2014bff15c832a466ea0
jxom <7336481+jxom@users.noreply.github.com>
2025-11-27 09:30:20 +1100

    chore: up snaps

--- src/viem/e2e.test.ts
+++ src/viem/e2e.test.ts
@@ -1462,7 +1462,6 @@ describe('relay', () => {
           ],
           "data": undefined,
           "feeToken": "0x20c0000000000000000000000000000000000001",
-          "keyAuthorization": null,
           "maxFeePerBlobGas": undefined,
           "nonceKey": 0n,
           "to": null,
